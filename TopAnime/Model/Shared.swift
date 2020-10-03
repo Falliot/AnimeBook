22c10808c0c04935416f0458c9d4ffd3cdb7be1a
Anton <pustovidko.anton@icloud.com>
2020-10-03 17:54:45 +0300

    fixed arrays

--- TopAnime/Model/Shared.swift
+++ TopAnime/Model/Shared.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 struct Genres {
-  static let animeGenres = ["Action", "Adventure", "Cars", "Comedy", "Dementia", "Mystery", "Drama", "Ecchi", "Fantasy", "Game", "Hentai", "Historical", "Horror", "Kids", "Magic", "Martical Arts", "Mecha", "Music", "Parody", "Samurai", "Romance", "School", "Sci Fi", "Shoujo", "Shoujo Ai", "Shounen", "Space", "Sports", "Super Power", "Vampire", "Yaoi", "Yuri", "Harem", "Slice Of Life", "Supernatural", "Military", "Police", "Psychological", "Thriller", "Seinen", "Josei"]
+  static let animeGenres = ["Action", "Adventure", "Cars", "Comedy", "Dementia", "Demons", "Mystery", "Drama", "Ecchi", "Fantasy", "Game", "Hentai", "Historical", "Horror", "Kids", "Magic", "Martical Arts", "Mecha", "Music", "Parody", "Samurai", "Romance", "School", "Sci Fi", "Shoujo", "Shoujo Ai", "Shounen", "Space", "Sports", "Super Power", "Vampire", "Yaoi", "Yuri", "Harem", "Slice Of Life", "Supernatural", "Military", "Police", "Psychological", "Thriller", "Seinen", "Josei"]
   static let mangaGenres = ["Action", "Adventure", "Cars", "Comedy", "Dementia", "Mystery", "Drama", "Ecchi", "Fantasy", "Game", "Hentai", "Historical", "Horror", "Kids", "Magic", "Martical Arts", "Mecha", "Music", "Parody", "Samurai", "Romance", "School", "Sci Fi", "Shoujo", "Shoujo Ai", "Shounen", "Space", "Sports", "Super Power", "Vampire", "Yaoi", "Yuri", "Harem", "Slice Of Life", "Supernatural", "Military", "Police", "Psychological", "Thriller", "Seinen", "Josei", "Gender Bender", "Thriller"]
 }
